4030644e9729848806bcf8278eb4d23f76f27953
Emmanuel Gomez <emgomez95@gmail.com>
2023-10-09 23:07:23 -0500

    first commit

---
+++ project/website/models.py
@@ -0,0 +1,17 @@
+from . import db
+import flask_login
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, flask_login.UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
